4d2379d2a98b15f3c9d84daed0b605c414125adc
MiguelGonzalezAcera <mga1192@gmail.com>
2023-03-30 16:40:56 +0200

    cleaned the clustering scripts

--- Rscripts/clustering.r
+++ Rscripts/clustering.r
@@ -7,27 +7,32 @@ suppressPackageStartupMessages(library(gplots))
 suppressPackageStartupMessages(library(circlize))
 suppressPackageStartupMessages(library(optparse))
 
-as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
-
-# NOTE: this chunk of code shall be replaced by a genelist input.
 # Might be a table from clusterProfiler, the raw genelist, or might be other source
-option_list = list(
-        make_option("--heatmap", type="character",
-                    help="Heatmap of the coverage of the genes over the samples"),
-        make_option("--counts", type="character",
-                    help="An r object with the normalized counts. Produced in the DE script."),
-        make_option("--genelist", type="character",
-                    help='List of genes to analyze. Ensembl coding'),
-        make_option("--organism", type="character", default= "mouse",
-                    help="Organism analyzed. Available = human, mouse. Default = mouse"),
-        make_option("--dims", type="character", default= "2000,2000",
-                    help="Dimensions of the plot in pixels. Default = 2000,2000"),
-        make_option("--design", type="character", default = "",
-                    help="Organism analyzed. Available = human, mouse. Default = mouse")
+option_list <- list(
+        make_option("--heatmap", type = "character",
+                    help = "Heatmap of the coverage of the genes over the samples"),
+        make_option("--counts", type = "character",
+                    help = "An r object with the normalized counts. Produced in the DE script."),
+        make_option("--genelist", type = "character",
+                    help = "List of genes to analyze. Ensembl coding"),
+        make_option("--organism", type = "character", default = "mouse",
+                    help = "Organism analyzed. Available = human, mouse. Default = mouse"),
+        make_option("--dims", type = "character", default = "2000,2000",
+                    help = "Dimensions of the plot in pixels. Default = 2000,2000"),
+        make_option("--colors", type = "character", default = "blue,white,red",
+                    help = "Colors for the heatmap, from lower to higher. Default = blue,white,red"),
+        make_option("--limits", type = "character", default = "-2,0,2",
+                    help = "Limits and center for the color scale. Default = -2,0,2"),
+        make_option("--cluster_cols", type = "character", default = "FALSE",
+                    help = "Enable column clustering. Options = TRUE, FALSE. Default = FALSE"),
+        make_option("--cluster_row", type = "character", default = "FALSE",
+                    help = "Enable row clustering. Options = TRUE, FALSE. Default = FALSE"),
+        make_option("--design", type = "character", default = "",
+                    help = "Organism analyzed. Available = human, mouse. Default = mouse")
 )
 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
 # Load R scripts
 source("Rscripts/Rfunctions.R")
@@ -40,23 +45,23 @@ load(opt$counts)
 
 # Select the columns for the design if there were any
 if (opt$design != "") {
-        sampleTableSingle = read.table(opt$design, fileEncoding = "UTF8")
+        sampleTableSingle <- read.table(opt$design, fileEncoding = "UTF8")
 
-        df_norm <- df_norm[c(rownames(sampleTableSingle) ,'Genename')]
+        df_norm <- df_norm[c(rownames(sampleTableSingle), "Genename")]
 }
 
 # Read the gene file
-genes = readLines(opt$genelist)
+genes <- readLines(opt$genelist)
 
 # Transform the ensembl names into gene symbol.
 df_norm$Genename <- rownames(df_norm)
 
 # Get rows in the list of genes
 m <- match(df_norm$Genename, genes)
-clust_df <- df_norm[!is.na(m),][order(na.omit(m)),]
-#clust_df <- df_norm[df_norm$Genename %in% genes, ,drop=FALSE]
-clust_df$Genename = NULL
+clust_df <- df_norm[!is.na(m), ][order(na.omit(m)), ]
+clust_df$Genename <- NULL
 
+# Check if enough the selected genes are in the provided table
 if (dim(clust_df)[1] <= 1) {
         print(sprintf("Could not find enough genes expressed for marker %s.", opt$genelist))
         opt <- options(show.error.messages=FALSE)
@@ -64,50 +69,83 @@ if (dim(clust_df)[1] <= 1) {
         quit(save = "no")
 }
 
+# Change names to gene symbol
 rows_hm <- as.character(mapIds(database, as.character(rownames(clust_df)),
-                               'SYMBOL', 'ENSEMBL'))
-# new <- 1000:2000
+                               "SYMBOL", "ENSEMBL"))
+
+# Replace those names without symbol in the databse package with the ensemblid
 rows_hm[is.na(rows_hm)|duplicated(rows_hm)] <- rownames(clust_df)[is.na(rows_hm)|duplicated(rows_hm)]
-        #paste("Unk",new[1:sum(is.na(rows_hm))], sep="")
-rownames(clust_df) <- rows_hm
 
 # Transform matrix to numeric
-cdf = clust_df
+# Copy just in case
+cdf <- clust_df
 
+# Columns of the dataframe are factors, so just transforming into matrix will fuck up the data.
 # Solution from: https://stackoverflow.com/questions/27528907/how-to-convert-data-frame-column-from-factor-to-numeric
 indx <- sapply(cdf, is.factor)
 cdf[indx] <- lapply(cdf[indx], function(x) as.numeric(as.character(x)))
 
+# Establish the symbols as rows
 rownames(cdf) <- rows_hm
 
-# Quick fix for column clustering in case some values are equal
-a <- cor(log(cdf + 1), method='pearson')
-rownames(cdf) <- rows_hma[is.na(a)] = 0
+# Cluster columns if requested
+if (as.logical(opt$cluster_cols)) {
+        # Quick fix for column clustering in case some values are equal
+        a <- cor(log(cdf + 1), method = "pearson")
+        rownames(cdf) <- rows_hma[is.na(a)] <- 0
 
-# Quick fix for the row clustering in case some values are equal
-b <- cor(log(t(cdf)), method='pearson')
-b[is.na(b)] = 0
+        # Perform the clustering analysis over the table
+        # Tree construction
+        cclust <- hclust(as.dist(1 - a), method = "complete")
+} else {
+        # Assign the FALSe value to the variable
+        cclust <- as.logical(opt$cluster_cols)
+}
 
-# Perform the clustering analysis over the table
-# Tree construction (rows and columns)
-hr <- hclust(as.dist(1-b), method="complete")
-hc <- hclust(as.dist(1-a), method="complete")
+# Cluster rows if requested
+if (as.logical(opt$cluster_rows)) {
+        # Quick fix for the row clustering in case some values are equal
+        b <- cor(log(t(cdf)), method = "pearson")
+        b[is.na(b)] <- 0
 
-# Establish colors
-color <- colorRamp2(c(-2, 0, 2), c("blue", "white", "red"))
+        # Perform the clustering analysis over the table
+        # Tree construction
+        rclust <- hclust(as.dist(1 - b), method = "complete")
+} else {
+        rclust <- as.logical(opt$cluster_rows)
+}
 
-png(file=opt$heatmap, width = int(strsplit(opt$dims, ',')[0]), height = int(strsplit(opt$dims, ',')[1]), res = 300)
-# Mount the heatmap
-#<TO_DO>: Add the title of the plot, according to whatever
-Heatmap(t(scale(t(log(cdf + 1)))), cluster_rows = FALSE,
-        cluster_columns = FALSE,
-        col=color, column_dend_height = unit(5, "cm"),
-        row_names_gp = gpar(fontsize = (90/length(genes)+5)),
+# Establish colors and limits of the gradient
+color <- colorRamp2(
+        c(
+                int(strsplit(opt$limits, ", ")[0]),
+                int(strsplit(opt$limits, ", ")[1]),
+                int(strsplit(opt$limits, ", ")[2])
+        ),
+        c(
+                strsplit(opt$colors, ", ")[0],
+                strsplit(opt$colors, ", ")[1],
+                strsplit(opt$colors, ", ")[2]
+        )
+)
+
+# Open the image with the given parameters
+png(
+        file = opt$heatmap,
+        width = int(strsplit(opt$dims, ",")[0]),
+        height = int(strsplit(opt$dims, ",")[1]),
+        res = 300
+)
+# Mount the heatmap with the respective transformations
+Heatmap(t(scale(t(log(cdf + 1)))), cluster_rows = rclust,
+        cluster_columns = cclust,
+        col = color, column_dend_height = unit(5, "cm"),
+        row_names_gp = gpar(fontsize = (90 / length(genes) + 5)),
         row_dend_width = unit(2, "cm"), show_row_names = TRUE)
-# dev.off()
+dev.off()
 
 # Save environment
-save.image(file=gsub(".png",".RData",opt$heatmap, fixed = TRUE))
+save.image(file = gsub(".png", ".RData", opt$heatmap, fixed = TRUE))
 
 # Save versions
-get_versions(gsub(".png","_versions.tsv",opt$heatmap, fixed = TRUE))
+get_versions(gsub(".png", "_versions.tsv", opt$heatmap, fixed = TRUE))
--- python_scripts/clustering_heatmap.py
+++ python_scripts/clustering_heatmap.py
@@ -1,63 +1,87 @@
 import argparse
 import logging
 import os
-import glob
-import json
-import subprocess
 import pandas as pd
 import python_scripts.python_functions as pf
 
 
 def clustering_heatmap(config, tool_name):
-    """Get the
-    """
+    # Report the start in the log
     logging.info(f'Starting {tool_name} process')
 
-    out_dir = "/".join(config['tools_conf'][tool_name]['output']['heatmap'].split('/')[0:-1])
-
+    # Extract the inputs to variables
+    # R object with the counts
     norm_counts = config['tools_conf'][tool_name]['input']['norm_counts']
+    # Table with the counts
     norm_counts_tab = norm_counts.replace(".Rda",".tsv")
-    heatmap = config['tools_conf'][tool_name]['output']['heatmap']
-    norm_counts_res = heatmap.replace('_heatmap.png','_NormCounts.tsv')
-    organism = config['options']['organism']
+    # Design file
     design = config['tools_conf'][tool_name]['input']['design']
+
+    # Extract the outputs
+    # Result heatmap path
+    heatmap = config['tools_conf'][tool_name]['output']['heatmap']
+    # Result path for accessory table
+    norm_counts_res = heatmap.replace('_heatmap.png','_NormCounts.tsv')
+    # Out directory
+    out_dir = "/".join(heatmap.split('/')[0:-1])
+
+    # Extract the other parameters
+    # Organism
+    organism = config['options']['organism']
+    # Dimensions of the plot
     dimensions = config['tools_conf'][tool_name]['tool_conf']['dimensions']
 
     # Create the command to run the pca R script
     command = ""
+    # Make the outdir if it doesn't exist
     if not os.path.exists(out_dir):
         command += f"mkdir {out_dir};"
 
+    # Get the genelist, if provided, or extract one using simple filters from the DE files
+    #<TODO>: I think the first option only makes sense if it is a standalone run, but I think this was to be removed, so...
     if 'genelist' in config['tools_conf'][tool_name]['input']:
+        # Get the genelist file from inputs
         genelist_path = config['tools_conf'][tool_name]['input']['genelist']
     else:
+        # Get the DE path from inputs
         out_dir_DE = "/".join(config['tools_conf'][tool_name]['input']['DEtouched'].split('/')[0:-1])
+        # Extract the design dictionary
         samples = config['comparisons']
 
+        # Init the list of genes
         genes_list = []
 
+        # Iter through the samples and controls
         for control in samples:
             sample_ids = samples[control].split(",")
             for sample in sample_ids:
+                # Construct the path for the DE table using the path and the project name
                 id_sample = out_dir_DE + "/" + config['project'] + "_" + f"{sample}_{control}.tsv"
 
+                # Load and select the genes that are significant
                 tdf = pd.read_csv(id_sample, sep='\t', index_col=None)
                 genes = tdf[tdf['padj'] < 0.05]["EnsGenes"].tolist()
 
+                # Add the selection to the list of genes
                 genes_list += genes
 
+        # Remove duplicated genes
         genes_list = list(set(genes_list))
 
+        # Save the list of genes to a file, to grep it later
         genelist_path = f"{out_dir_DE}/significant_genes.txt"
         gene_file = open(genelist_path,"w")
         for gene in genes_list:
             gene_file.write(f"{gene}\n")
         gene_file.close()
 
+    # Make the command for the clustering
     command += f'Rscript Rscripts/clustering.r --heatmap {heatmap} --counts {norm_counts} --genelist {genelist_path} --organism {organism} --design {design} --dims {dimensions}; '
 
+    # Slice the table with the selected genes
     command += f'head -n +1 {norm_counts_tab} | awk \'{{print \"EnsemblID\\t\" $0}}\' > {norm_counts_res}; grep -f {genelist_path} {norm_counts_tab} >> {norm_counts_res}; '
 
+    # log and run the command
     logging.info(f'Running command: {command}')
     pf.run_command(command)
 
