221e0e20bb0934bee4fe2f11132df6a0b841f231
MiguelGonzalezAcera <mga1192@gmail.com>
2023-03-31 12:23:06 +0200

    Cleaned the GSEAplot a bit, even if it's not included in the pipeline

--- Rscripts/GSEA.r
+++ Rscripts/GSEA.r
@@ -4,19 +4,19 @@ suppressPackageStartupMessages(library(DESeq2))
 suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(ggplot2))
 
-option_list = list(
-  make_option("--genegroup", type="character",
-              help="Path for the groups of genes, in particular entrez table format"),
-  make_option("--in_obj", type="character",
-              help="Robject with the DE analysis. Rda extension"),
-  make_option("--gseaplot", type="character",
-              help="Path for the GSEAplot. png extension"),
-  make_option("--organism", type="character", default= "mouse",
-              help="Organism analyzed. STR. Available = human, mouse. Default = Mouse")
+option_list <- list(
+  make_option("--genegroup", type = "character",
+              help = "Path for the groups of genes, in particular entrez table format"),
+  make_option("--in_obj", type = "character",
+              help = "Robject with the DE analysis. Rda extension"),
+  make_option("--gseaplot", type = "character",
+              help = "Path for the GSEAplot. png extension"),
+  make_option("--organism", type = "character", default = "mouse",
+              help = "Organism analyzed. STR. Available = human, mouse. Default = Mouse")
 )
 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
 
 # Load the R object with the result
 load(opt$in_obj)
@@ -30,25 +30,25 @@ database <- select.organism(opt$organism)
 # Transform the object into a named genelist (all of the genes)
 geneList <- res$log2FoldChange
 names(geneList) <- as.character(mapIds(database, as.character(rownames(res)),
-                                       'ENTREZID', 'ENSEMBL'))
+                                       "ENTREZID", "ENSEMBL"))
 
 # Read the custom groups (column 1: name of the group, and column 2, gene id in entrez)
-groups = read.table(opt$genegroup, fileEncoding = "UTF8")
+groups <- read.table(opt$genegroup, fileEncoding = "UTF8")
 
 # Do the gene set enrichment analysis
-z = GSEA(sort(geneList,decreasing=T), TERM2GENE = groups, pvalueCutoff = 1, minGSSize = 5)
+z <- GSEA(sort(geneList,decreasing=T), TERM2GENE = groups, pvalueCutoff = 1, minGSSize = 5)
 
 # Plot the result of the GSA
-if (length(rownames(as.data.frame(z))) >= 10){
-  len = 10
+if (length(rownames(as.data.frame(z))) >= 10) {
+  len <- 10
 } else {
-  len = length(rownames(as.data.frame(z)))
+  len <- length(rownames(as.data.frame(z)))
 }
 
 # Save enrichment table
 write.table(as.data.frame(z), file=gsub(".png",".tsv",opt$gseaplot, fixed = TRUE),sep="\t",row.names = FALSE)
 
 # Make and save the plot
-png(file=opt$gseaplot, width = 2000, height = 2000, res = 200)
-gseaplot2(z, geneSetID = 1, color="red", pvalue_table = FALSE, base_size = 24)
+png(file = opt$gseaplot, width = 2000, height = 2000, res = 200)
+gseaplot2(z, geneSetID = 1, color = "red", pvalue_table = FALSE, base_size = 24)
 dev.off()
