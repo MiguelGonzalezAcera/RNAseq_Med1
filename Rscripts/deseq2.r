ac29fdc9b14c17bc4dee8804d4fb2012f835daa4
MiguelGonzalezAcera <mga1192@gmail.com>
2023-04-04 12:41:34 +0200

    Cleaned some things in the deseq2 r script

--- Rscripts/deseq2.r
+++ Rscripts/deseq2.r
@@ -35,9 +35,13 @@ database <- select.organism(opt$organism)
 sampleTableSingle <- read.table(opt$design, fileEncoding = "UTF8")
 
 # Read the table containing the counts
-Counts_tab <- read.table(opt$counts, fileEncoding = "UTF8", header=TRUE)
+Counts_tab <- read.table(opt$counts, fileEncoding = "UTF8", header = TRUE)
+
+# Move the gene IDs as row names
 row.names(Counts_tab) <- Counts_tab$Geneid
 Counts_tab$Geneid <- NULL
+
+# Select the columns specified in the provided design and resort the genes
 Counts_tab <- Counts_tab[, row.names(sampleTableSingle)]
 Counts_tab <- Counts_tab[order(row.names(Counts_tab)), ]
 
@@ -46,9 +50,10 @@ sampleTableSingle$rn <- row.names(sampleTableSingle)
 control_samples <- sampleTableSingle[sampleTableSingle$Tr1 == opt$control,][['rn']]
 
 # Design model matrix
-#design <- model.matrix( ~ as.character(sampleTableSingle[,1]) + as.character(sampleTableSingle[,2]))
 Tr1 <- relevel(factor(sampleTableSingle[, 1]), opt$control)
 design <- model.matrix(~ Tr1)
+
+# --------------------------------------------------------------
 
 # Create the experiment from a SummarizedExperiment object
 dss <- DESeqDataSetFromMatrix(countData = Counts_tab,
@@ -56,9 +61,9 @@ dss <- DESeqDataSetFromMatrix(countData = Counts_tab,
                               design = design)
 
 # Avoid normalization
-#sizeFactors(dss) <- 1
+# sizeFactors(dss) <- 1
 
-# Save the universe (of genes)
+# Save the universe (of genes). This is important for downstream analyses
 save(dss, file = gsub(".Rda", "_universe.Rda", opt$out_obj, fixed = TRUE))
 
 # filter the counts by number
@@ -68,41 +73,34 @@ dss <- dss[keep, ]
 # Run the analysis
 dds <- DESeq(dss, betaPrior = FALSE)
 
-# Check the names of the main contrasts
-# print(resultsNames(dds))
-
-# Plot the dispersion of the set
-# plotDispEsts(dds)
+# --------------------------------------------------------------
 
 # Save the normalized counts
-# <TO_DO>: The header is odd in the file, so check the samples or load the whole object when working with the normalized counts.
+# Get the table from the result
 norm_counts <- counts(estimateSizeFactors(dds), normalized = TRUE)
+
+# Get the names of the columns
 norm_counts_colnames <- colnames(norm_counts)
+# Add the gene names as a new column
 norm_counts <- cbind(norm_counts, as.character(mapIds(database, as.character(rownames(norm_counts)), 'SYMBOL', 'ENSEMBL')))
+# Rename the columns with the new name
 colnames(norm_counts) <- c(norm_counts_colnames, "Genename")
 
+# Save the object both as table and as R object
 write.table(norm_counts, file=gsub(".Rda","_norm_counts.tsv", opt$out_obj, fixed = TRUE), sep="\t")
 df_norm <- as.data.frame(norm_counts)
 save(df_norm, file = gsub(".Rda", "_norm_counts.Rda", opt$out_obj, fixed = TRUE))
 
+# --------------------------------------------------------------
+
+# Get the EnsemblIDs from the counts table
 df_norm$EnsGenes <- rownames(df_norm)
 
-#<TO_DO>: Now, this is doubtful, because now would be time to do the contrasts.
-# To do these accurately, we have to consider, number of factors, levels of each factor,
-#  interaction of the factors, and which factors interact.
-# Maybe define the design via input? (Galaxy does it like this)
-
-# Anyway, the output should be a filtered DESeq2 result object and a table with
-# the results (named after the contrast used)
-
-# Contrast may vary. Probably need to use loop for all contrasts,
-# even more if there is interaction. <TO_DO>: Also, use given threshold
-# res <- results(dds, alpha = 0.001, contrast=c("Treatment","REC_FUL","HEALTHY"))
-
-# Split the comparisons and run the loop of tables
+# Split the comparisons and run the loop to get each table
 for (sample in strsplit(opt$comparisons, ",")[[1]]){
   # Using the names provided in the input as the samples, run this as a loop
   res <- results(dds, name = paste("Tr1", sample, sep = ""), cooksCutoff = FALSE)
+
   # Save the full result object
   # Contrast name will be replaced by the sample and controls
   res_name <- paste(paste("", sample, opt$control, sep = "_"), "Rda", sep = ".")
@@ -132,7 +130,7 @@ for (sample in strsplit(opt$comparisons, ",")[[1]]){
   # Subset design table with sample to get vector
   sample_samples <- sampleTableSingle[sampleTableSingle$Tr1 == sample, ][["rn"]]
 
-  # Merge res table with the counts of its samples
+  # Merge res table with the counts of its samples and controls
   resdf_wcounts <- merge(resdf, df_norm[,c("EnsGenes", control_samples, sample_samples)], by="EnsGenes", all.x=TRUE)
 
   # filter by normalized counts in order to remove false positives
